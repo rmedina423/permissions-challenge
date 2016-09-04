c476d8f4788dad96235b3587f98aa609648e099c
Ryan M. Medina <rmedina423@gmail.com>
2016-09-04 08:40:46 -0700

    add semicolons

--- js/index.js
+++ js/index.js
@@ -1,78 +1,77 @@
-/* jshint node: true, asi: true */
-'use strict'
+'use strict';
 
 var $ = require('jquery');
-var template = require('./template.js')
+var template = require('./template.js');
 
 $(function () {
 
-	var getPermissions = $.get('http://localhost:3000/permissions')
-	var getNames = $.get('http://localhost:3000/users')
+	var getPermissions = $.get('http://localhost:3000/permissions');
+	var getNames = $.get('http://localhost:3000/users');
 
 	$('button').on('click', function (event) {
-		event.preventDefault()
+		event.preventDefault();
 
-		$('li').remove()
-		$('h3').remove()
+		$('li').remove();
+		$('h3').remove();
 
 		getNames
 			.done(function (users) {
 				users.forEach(function (user) {
-					$('main').append(renderName(user))
-				})
-			})
-	})
+					$('main').append(renderName(user));
+				});
+			});
+	});
 
 	$('main').on('click', 'a', function (event) {
-		event.preventDefault()
+		event.preventDefault();
 
-		var id = $(this).parent().data('id')
+		var id = $(this).parent().data('id');
 
-		$('li').remove()
+		$('li').remove();
 
 		getNames
 			.done(function (users) {
 				users.forEach(function (user) {
 					if (id === user.id) {
-						$('button').after(renderTitle(user))
+						$('button').after(renderTitle(user));
 					}
-				})
-			})
+				});
+			});
 
 		getPermissions
 			.done(function (permissions) {
 				permissions.forEach(function (permission) {
 					if(id === permission.userId) {
-						$('main').append(renderPermissions(permission))
+						$('main').append(renderPermissions(permission));
 					}				
-				})
-			})
+				});
+			});
 
-	})
+	});
 
 	function renderName(user) {
 
 		var theData = {
 			user: user
-		}
+		};
 
-		return template.userName(theData)
+		return template.userName(theData);
 	}
 
 	function renderPermissions(permission) {
 		var theData = {
 			permission: permission
-		}
+		};
 
-		return template.permissions(theData)
+		return template.permissions(theData);
 	}
 
 	function renderTitle(user) {
 		var theData = {
 			user: user
-		}
+		};
 
-		return template.title(theData)
+		return template.title(theData);
 	}
 
-})
+});
--- js/template.js
+++ js/template.js
@@ -2,12 +2,12 @@
 
 var Handlebars = require('hbsfy/runtime')
 
-var userName = require('./../templates/userName.handlebars')
-var permissions = require('./../templates/permissions.handlebars')
-var title = require('./../templates/title.handlebars')
+var userName = require('./../templates/userName.handlebars');
+var permissions = require('./../templates/permissions.handlebars');
+var title = require('./../templates/title.handlebars');
 
 module.exports = {
 	userName: userName,
 	permissions: permissions,
 	title: title,
-}
+};
